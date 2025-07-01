06da216dff4def2f0832b7e753dabdcda8240fe1
Dhakshana <sundarichandran1975@gmail.com>
2025-07-01 21:55:03 +0530

    Updated portfolio title and layout

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
-
-
-  plugins: [react()],
-})
+  base: "/My-React-portfolio-website/", // your repo name
+});
