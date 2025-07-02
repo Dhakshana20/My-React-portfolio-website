2f5a2a512935e6af65c02b58a7a0679319917ced
Dhakshana <sundarichandran1975@gmail.com>
2025-07-02 11:13:01 +0530

    Updated Footer and Home styles for mobile responsiveness

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+// src/Components/Navbar.jsx
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -7,7 +8,7 @@ function Navbar({ toggleTheme, darkMode }) {
 
   return (
     <nav className="navbar">
-      <div className="logo">Dhakshana R</div> {/* Removed 🌙/☀️ icon */}
+      <div className="logo">Dhakshana </div>
 
       <div className={`nav-links ${menuOpen ? "open" : ""}`}>
         <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
@@ -15,6 +16,17 @@ function Navbar({ toggleTheme, darkMode }) {
         <Link to="/skills" onClick={() => setMenuOpen(false)}>Skills</Link>
         <Link to="/projects" onClick={() => setMenuOpen(false)}>Projects</Link>
         <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+      </div>
+
+      <div className="actions">
+        
+
+        <button
+          className="menu-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
       </div>
     </nav>
   );
