be16a6434884d5a0137aaf9c44410bc5c73a0126
Dhakshana <sundarichandran1975@gmail.com>
2025-07-02 10:39:53 +0530

    Updated components like About, Navbar, etc.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,20 +1,18 @@
 // src/components/About.jsx
-import { motion } from "framer-motion";
 import "./About.css";
 
 function About() {
   return (
     <section className="about-section">
-      <motion.div
-        className="about-content"
-        initial={{ opacity: 0, y: 100 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-      >
+      <div className="about-content fade-in">
         <h2>About Me</h2>
         <p>
-I’m Dhakshana, a curious and creative developer with a passion for building efficient and user-friendly applications. I enjoy solving real-world problems through code and am continuously learning to become a skilled and versatile software developer.        </p>
-      </motion.div>
+          I’m Dhakshana, a curious and creative developer with a passion for building
+          efficient and user-friendly applications. I enjoy solving real-world problems
+          through code and am continuously learning to become a skilled and versatile
+          software developer.
+        </p>
+      </div>
     </section>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// vite.config.js
 export default defineConfig({
-  base: "/My-React-portfolio-website/", // your repo name
-});
+
+
+  plugins: [react()],
+})
